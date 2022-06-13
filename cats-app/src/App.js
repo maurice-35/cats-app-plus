f3c8c31021f06914d1cf1e2397a20e816449413b
mauricekollewe <kollewem.35@gmail.com>
2022-06-13 09:06:57 +0100

    getData from api

--- cats-app/src/App.js
+++ cats-app/src/App.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { Card, ListGroup } from 'react-bootstrap'
 
 const App = () => {
 
 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('https://api.thecatapi.com/v1/breeds')
+        console.log(data);
+      } catch (err) {
+    };
+  }
+    getData();
+  }, []);
+
+
   return (
-    <div>
-      <h1>Welcome Home!</h1>
-    </div>
+    <section>
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <Card.Body>
+            <Card.Title>Name</Card.Title>
+            <Card.Text>
+              Description
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Energy-level</ListGroup.Item>
+            <ListGroup.Item>Dog friendly</ListGroup.Item>
+            <ListGroup.Item>Life-span</ListGroup.Item>
+            <ListGroup.Item>Origin</ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </div>
+    </section>
   )
 }
 
