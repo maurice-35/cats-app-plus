2ba0baadf5d3fc6af94a7167775442d58b8a434f
mauricekollewe <kollewem.35@gmail.com>
2022-07-31 19:38:43 +0100

    update CardIndex.test

--- cats-app/src/component/cats/CatIndex.test.js
+++ cats-app/src/component/cats/CatIndex.test.js
@@ -31,40 +31,27 @@ afterEach(() => {
 	container = null
 })
 it('renders breed data', async () => {
-		const fakeBreed = {
-			image: "",
-			origin: "",
-			name: "",
-			description: "Lovely cat",
-			energy_level: 4,
-			child_friendly: 3,
-			dog_friendly: 2,
-			intelligence: 1,
-			social_needs: 3,
-			health_issues: 2,
-			adaptability: 4,
-			vocalisation: 3,
-			affection_level: 2,
-			temperament: 'Active',
-			grooming: 3,
-			life_span: 6 - 7,
-			weight: "",
-		}
-		jest.spyOn(global, "fetch").mockImplementation(() => 
+	const fakeBreed = {
+		image: "",
+		origin: "",
+		name: "",
+		weight: "",
+	}
+	jest.spyOn(global, "fetch").mockImplementation(() =>
 		Promise.resolve({
 			json: () => Promise.resolve(fakeBreed)
 		})
-		)
-		
-		// eslint-disable-next-line testing-library/no-unnecessary-act
-		await act(async () => {
-			fireEvent.click(container)
-			render(<CatIndex id="123" />, container)
-		})
+	)
 
-		expect(container.textContent).toContain(fakeBreed.name)
-		expect(container.textContent).toContain(fakeBreed.origin)
-		expect(container.textContent).toContain(fakeBreed.image)
-		expect(container.textContent).toContain(fakeBreed.weight)
-		global.fetch.mockRestore()
+	// eslint-disable-next-line testing-library/no-unnecessary-act
+	await act(async () => {
+		fireEvent.click(container)
+		render(<CatIndex id="123" />, container)
+	})
+
+	expect(container.textContent).toContain(fakeBreed.name)
+	expect(container.textContent).toContain(fakeBreed.origin)
+	expect(container.textContent).toContain(fakeBreed.image)
+	expect(container.textContent).toContain(fakeBreed.weight)
+	global.fetch.mockRestore()
 })
